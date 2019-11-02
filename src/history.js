60cd7eda521ff85b45759cdddf5e5f591e464e40
Tres Comas <trescomas@icloud.com>
2019-11-01 21:13:02 -0500

    update

--- src/history.js
+++ src/history.js
@@ -1,6 +1,6 @@
 import createHistory from 'history/createBrowserHistory';
 
 // Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory()
+const history = createHistory({ basename: '/jegyek' })
 
 export default history;
