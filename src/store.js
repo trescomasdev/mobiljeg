0be1c6b3a8a0fc785c9dfbe5425f845cd323ff84
Tres Comas <trescomas@icloud.com>
2019-11-01 21:07:54 -0500

    update

--- src/store.js
+++ src/store.js
@@ -1,15 +1,15 @@
-import { applyMiddleware, createStore} from 'redux';
-import { logger } from "redux-logger";
-import thunk from 'redux-thunk';
-import promise from "redux-promise-middleware";
+import { applyMiddleware, createStore} from 'redux'
+// import { logger } from "redux-logger"
+import thunk from 'redux-thunk'
+import promise from "redux-promise-middleware"
 import { routerMiddleware } from 'react-router-redux'
 
 import history from './history'
-import reducer from "./reducers";
+import reducer from "./reducers"
 
 // Build the middleware for intercepting and dispatching navigation actions
 const routeMiddleware = routerMiddleware(history)
 
-const middleware = applyMiddleware(promise(), thunk, logger, routeMiddleware);
+const middleware = applyMiddleware(promise(), thunk, routeMiddleware)
 
-export default createStore(reducer, middleware);
+export default createStore(reducer, middleware)
