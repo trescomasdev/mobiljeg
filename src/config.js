2c65d49d8961dcd61fc0c4d0c29b66e9a2bd20fa
Tres Comas <trescomas@icloud.com>
2019-11-25 22:43:40 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+
 const REMOTE_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_REMOTE_URL  : process.env.REACT_APP_REMOTE_TEST_URL
 const BARIONURL = process.env.NODE_ENV === 'development' || process.env.REACT_APP_BARION_TEST_MODE === "true" ? "api.test.barion.com" : "api.barion.com"
 const BARIONKEY = process.env.NODE_ENV === 'development' || process.env.REACT_APP_BARION_TEST_MODE === "true" ? "c7fac0dccb2748c6814584a61f18bd7d" : "bb33c0ea62a24af8b1241ffa33f7bcf5"
